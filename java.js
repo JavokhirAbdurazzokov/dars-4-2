f6bddfd9969f8a457d4138142d3a7ee649f3727d
JavokhirAbdurazzokov <168398916+JavokhirAbdurazzokov@users.noreply.github.com>
2024-07-02 20:19:38 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+let ism =prompt("String type da ma'lumot kiriting");
+let yosh= +prompt("Number type da ma'lumot kiriting");
+let Bolean= +prompt("Bolean type da ma'lumot kiriting");
+
+confirm("Hamma ma'limot togrimi")
+alert(
+    "siz kiritgan "+
+    ism +
+    "Stiring type da ! \n siz kiritilgan "
+    + yosh +
+    "Number type da!" 
+    + "Siz kiritgan" +
+    Bolean +
+    "Bolean type da"
+)
